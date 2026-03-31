7028503a86995c96bd6002ec1db7d68084dd94f9
Muumi <ulmer.d@outlook.com>
2026-03-31 22:25:55 +0200

    Name

--- src/main/java/net/beetle/naturestreasures/item/ModItems.java
+++ src/main/java/net/beetle/naturestreasures/item/ModItems.java
@@ -11,10 +11,10 @@ import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
 public class ModItems {
-    public static final Item TEST= registerItem("test", new Item(new FabricItemSettings()));
+    public static final Item TEST1= registerItem("test1", new Item(new FabricItemSettings()));
 
     private static void addItemsToIngredientItemGroup(FabricItemGroupEntries entries) {entries
-            .add(TEST);
+            .add(TEST1);
     }
 
     private static Item registerItem(String name, Item item) {
