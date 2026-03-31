5abecd59accbf695beca7928ba39dd67b6295c27
Muumi <ulmer.d@outlook.com>
2026-04-01 00:11:51 +0200

    - New Creativ Tab

--- src/main/java/net/beetle/naturestreasures/NaturesTreasures.java
+++ src/main/java/net/beetle/naturestreasures/NaturesTreasures.java
@@ -1,5 +1,6 @@
 package net.beetle.naturestreasures;
 
+import net.beetle.naturestreasures.item.ModItemGroups;
 import net.beetle.naturestreasures.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
@@ -14,5 +15,6 @@ public class NaturesTreasures implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		ModItems.registerModItems();
+		ModItemGroups.registerItemGroups();
 	}
 }
---
+++ src/main/java/net/beetle/naturestreasures/item/ModItemGroups.java
@@ -0,0 +1,26 @@
+package net.beetle.naturestreasures.item;
+
+import net.beetle.naturestreasures.NaturesTreasures;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+
+public class ModItemGroups {
+
+    public static final ItemGroup NATURES_TREASURES_TAB = Registry.register(Registries.ITEM_GROUP,
+            new Identifier(NaturesTreasures.MOD_ID, "natures_treasures_tab"),
+            FabricItemGroup.builder().displayName(Text.translatable("itemgroup.natures_treasures_tab"))
+                    .icon(() -> new ItemStack(ModItems.WOODLAND_DOR_BEETLE_ITEM)).entries((displayContext, entries) -> {
+                        entries.add(ModItems.ANT_ITEM);
+                        entries.add(ModItems.WOODLAND_DOR_BEETLE_ITEM);
+                    }).build());
+
+
+    public static void registerItemGroups() {
+        NaturesTreasures.LOGGER.info("Registering Item Groups for" + NaturesTreasures.MOD_ID);
+    }
+}
